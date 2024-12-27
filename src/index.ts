901bc55e4115bc7944b199b14563be4c65839569
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-27 22:56:08 +0300

    added prismaClient configuration and error middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import express, { Express, Request, Response } from "express";
+import { PORT } from "./secrets";
+
+import rootRouter from "./routes";
+import { PrismaClient } from "@prisma/client";
+import { errorMiddleware } from "./middlewares/errors";
 
 const app: Express = express();
+
+app.use(express.json());
+app.use(errorMiddleware);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
+app.use("/api", rootRouter);
+
+export const prismaClient = new PrismaClient({
+  log: ["query"],
+}); 
+
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
