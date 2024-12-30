8f100b5c5d6a2c212d6c363647a9f5687dfe16eb
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-30 23:27:08 +0300

    users schema validation

--- src/schema/users.ts
+++ src/schema/users.ts
@@ -10,3 +10,17 @@ export const LoginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
 });
+
+export const AddressSchema = z.object({
+  lineOne: z.string(),
+  lineTwo: z.string().optional(),
+  city: z.string(),
+  country: z.string(),
+  pinCode: z.string().length(6),
+});
+
+export const UpdateUserSchema = z.object({
+  name: z.string().optional(),
+  defaultShippingAddress: z.number().optional(),
+  defaultBillingAddress: z.number().optional(),
+});
