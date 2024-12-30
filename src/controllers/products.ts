882b3b4607d33be5703a9dfd75d44d8a7681d2ee
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-12-30 08:47:23 +0300

    updated error handling

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -2,27 +2,34 @@ import { NextFunction, Request, Response } from "express";
 import { prismaClient } from "..";
 import { CreateProductSchema } from "../schema/products";
 import { NotFoundException } from "../exceptions/not-found";
+import { UnprocessableEntityException } from "../exceptions/validation";
+import { ErrorCode } from "../exceptions/root";
 
 export const createProduct = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const validatedData = CreateProductSchema.parse(req.body);
-    const product = await prismaClient.product.create({
-      data: {
-        ...validatedData,
-        tags: validatedData.tags.join(","),
-      },
-    });
-    res.json(product);
-  } catch (error: any) {
-    res.status(400).json({
-      error: "Validation failed",
-      details: error.message,
-    });
+  const validatedData = CreateProductSchema.parse(req.body);
+  if (!validatedData || validatedData.price < 0) {
+    next(
+      new UnprocessableEntityException(
+        "Invalid Input!",
+        null,
+        ErrorCode.UNPROCESSABLE_ENTITY
+      )
+    );
   }
+  const product = await prismaClient.product.create({
+    data: {
+      ...validatedData,
+      tags: validatedData.tags.join(","),
+    },
+  });
+  if (!product) {
+    next(new NotFoundException("Product not found!", 404, null));
+  }
+  res.status(200).json(product);
 };
 
 export const updateProduct = async (
@@ -30,27 +37,29 @@ export const updateProduct = async (
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const validatedData = CreateProductSchema.parse(req.body);
-    const product = await prismaClient.product.update({
-      where: {
-        id: +req.params.id,
-      },
-      data: {
-        ...validatedData,
-        tags: validatedData.tags.join(","),
-      },
-    });
-    res.json(product);
-  } catch (error: any) {
-    return next(
-      new NotFoundException(
-        "Product not found or Invalid Input!",
-        404,
-        error.message
+  const validatedData = CreateProductSchema.parse(req.body);
+  if (!validatedData || validatedData.price < 0) {
+    next(
+      new UnprocessableEntityException(
+        "Invalid Input!",
+        null,
+        ErrorCode.UNPROCESSABLE_ENTITY
       )
     );
   }
+  const product = await prismaClient.product.update({
+    where: {
+      id: +req.params.id,
+    },
+    data: {
+      ...validatedData,
+      tags: validatedData.tags.join(","),
+    },
+  });
+  if (!product) {
+    next(new NotFoundException("Product not found!", 404, null));
+  }
+  res.status(200).json(product);
 };
 
 export const deleteProduct = async (
@@ -58,19 +67,15 @@ export const deleteProduct = async (
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const product = await prismaClient.product.delete({
-      where: {
-        id: +req.params.id,
-      },
-    });
-    res.json(product);
-  } catch (error: any) {
-    res.status(400).json({
-      error: "Validation failed",
-      details: error.message,
-    });
+  const product = await prismaClient.product.delete({
+    where: {
+      id: +req.params.id,
+    },
+  });
+  if (!product) {
+    next(new NotFoundException("Product not found!", 404, null));
   }
+  res.status(200).json(product);
 };
 
 export const getProductById = async (
@@ -78,19 +83,15 @@ export const getProductById = async (
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const product = await prismaClient.product.findUnique({
-      where: {
-        id: +req.params.id,
-      },
-    });
-    res.json(product);
-  } catch (error: any) {
-    res.status(400).json({
-      error: "Validation failed",
-      details: error.message,
-    });
+  const product = await prismaClient.product.findUnique({
+    where: {
+      id: +req.params.id,
+    },
+  });
+  if (!product) {
+    next(new NotFoundException("Product not found!", 404, null));
   }
+  res.status(200).json(product);
 };
 
 export const listProducts = async (
