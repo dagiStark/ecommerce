da16770ddc86c2ef45691b06faad03a9d560dbd8
dagiStark <160214504+dagiStark@users.noreply.github.com>
2025-01-01 22:23:53 +0300

    added order and carts routes to the main route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import authRoutes from "./auth";
 import productsRoutes from "./products";
 import usersRoutes from "./users";
+import ordersRoutes from "./order";
+import cartRoutes from "./carts";
 
 const rootRouter: Router = Router();
 
 rootRouter.use("/auth", authRoutes);
 rootRouter.use("/products", productsRoutes);
 rootRouter.use("/users", usersRoutes);
+rootRouter.use("/orders", ordersRoutes);
+rootRouter.use("/carts", cartRoutes);
 
 export default rootRouter;
